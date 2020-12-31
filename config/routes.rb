fc38f41d386a47bb659228388ba1766b8cf052b1
rsuazo <rsuazo2@gmail.com>
2020-12-31 15:17:59 -0800

    force login before actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_scope :user do
     authenticated :user do
-      root 'home#index', as: :authenticated_root
+      root 'users#show', as: :authenticated_root
     end
   
     unauthenticated do
