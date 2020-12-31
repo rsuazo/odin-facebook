4d5bab28ecc43856498f5b2279997de06336f3f3
rsuazo <rsuazo2@gmail.com>
2020-12-30 20:10:02 -0800

    generate users controller and update views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(email: "rsuazo@gmail.com", password: "testing", password_confirmation: "testing", first_name: "Robert", last_name: "Suazo", user_name: "rsuazo")
+
+User.create(email: "tsuazo@gmail.com", password: "testing", password_confirmation: "testing", first_name: "Tiffany", last_name: "Suazo", user_name: "tsuazo")
+
+User.create(email: "jsimpson@gmail.com", password: "testing", password_confirmation: "testing", first_name: "Jasmin", last_name: "Simpson", user_name: "jsimpson")
+
+User.create(email: "nsimpson@gmail.com", password: "testing", password_confirmation: "testing", first_name: "Nate", last_name: "Simpson", user_name: "nsimpson")
+
+User.create(email: "msuazo@gmail.com", password: "testing", password_confirmation: "testing", first_name: "Martin", last_name: "Suazo", user_name: "msuazo")
+
+User.create(email: "ruksuazo@gmail.com", password: "testing", password_confirmation: "testing", first_name: "Rukshana", last_name: "Suazo", user_name: "ruksuazo")
