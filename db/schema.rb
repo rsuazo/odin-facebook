a7e98b90254ba51551bfc513a32a591e0e6eb81f
rsuazo <rsuazo2@gmail.com>
2021-01-16 17:05:32 -0800

    implement posts model and controller and migrate DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_231137) do
+ActiveRecord::Schema.define(version: 2021_01_17_005804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_231137) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "requests", force: :cascade do |t|
@@ -51,4 +52,5 @@ ActiveRecord::Schema.define(version: 2021_01_11_231137) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
