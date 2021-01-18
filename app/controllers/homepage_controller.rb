68bba725843f29cfd42fe8fa8b1fdec28152a396
rsuazo <rsuazo2@gmail.com>
2021-01-18 14:59:16 -0800

    add post functionality

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,7 @@
 class HomepageController < ApplicationController
     def index
       @post = Post.new
+      @posts = current_user.posts
       @user = current_user
     end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+    def index 
+      @posts = current_user.posts
+    end
+
+    def create
+        @post = current_user.posts.build(title: params[:post][:title], body: params[:post][:body])
+          if @post.save
+              flash[:notice] = "Created Post!"
+              redirect_to homepage_index_path
+  
+          else
+              flash[:error] = "Unable to create post."
+              redirect_to users_path
+          end
+      end
 end
