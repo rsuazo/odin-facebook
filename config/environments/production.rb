befeacc67f7b910205e194c14496a618f4db6b42
rsuazo <rsuazo2@gmail.com>
2021-01-29 20:52:22 -0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { :host => "odin-facebook-suazo.herokuapp.com" }
+  # config.action_mailer.default_url_options = { :host => "odin-facebook-suazo.herokuapp.com" }
 
 
 end
